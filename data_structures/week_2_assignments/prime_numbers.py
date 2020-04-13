e505b69d6fa0e675f8a32ac4e3e316465e1dc743
EllieBui <ellie@gotitapp.co>
2020-04-13 23:37:42 +0700

    feat: Update week 2 assignment per feedback

--- data_structures/week_2_assignments/prime_numbers.py
+++ data_structures/week_2_assignments/prime_numbers.py
@@ -1,24 +1,22 @@
-import math
 
 prime_numbers = [2]
 number = 2
 
 while True:
-    i = 2
-    is_prime = 1
-    # For each number, divide it by each number in range(2 and its square root +1)
-    for i in range(2, math.ceil(math.sqrt(number)) + 1):
+    is_prime = True
+    # For each number, divide it by each number in list of prime number less than it
+    for i in prime_numbers:
         if number % i == 0:
-            # If number is divisible by any number in that range, it is not prime
+            # If number is divisible by any number already in list of prime numbers, it is not prime
             # Stop for loop to check for prime
-            is_prime -= 1
+            is_prime = False
             break
-        # If number is not divisible by each number in range, increment i by 1
+        # If number is not divisible by each number in list of prime numbers, increment i by 1
         i += 1
 
-    # If number is not divisible by any number in that range,
-    # is_prime should have value 1. Add it to the list.
-    if is_prime == 1:
+    # If number is not divisible by any number in list of prime numbers,
+    # is_prime should have value True. Add it to the list.
+    if is_prime:
         prime_numbers.append(number)
 
     # Once a number is checked, increment number by 1
@@ -29,8 +27,8 @@ while True:
     if len(prime_numbers) == 10001:
         break
 
-# Return the 10001st prime number
-print(prime_numbers[10000])
+# Return the 10001st prime number (last item in list of 10001 items)
+print(prime_numbers[-1])
 
 
 """
