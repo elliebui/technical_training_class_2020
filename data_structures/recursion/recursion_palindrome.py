d0adf5dcf28681eabdb99e353f15b8466f9ec5e8
EllieBui <ellie@gotitapp.co>
2020-04-14 23:48:02 +0700

    fix: update recursion is_palindrome

--- data_structures/recursion/recursion_palindrome.py
+++ data_structures/recursion/recursion_palindrome.py
@@ -9,11 +9,9 @@ def is_palindrome(input):
 
     if len(input) == 0 or len(input) == 1:
         return True
-    else:
-        if input[0] == input[-1]:
-            return is_palindrome(input[1:-1])
-        else:
-            return False
+    elif input[0] != input[-1]:
+        return False
+    return is_palindrome(input[1:-1])
 
 
 # Test Cases
