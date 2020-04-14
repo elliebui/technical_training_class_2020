d611c13b5ed0a1401e3bc38bb65fc046d496f88f
EllieBui <ellie@gotitapp.co>
2020-04-15 00:22:07 +0700

    fix: update is_prime exercise

--- data_structures/week_2_assignments/prime_numbers.py
+++ data_structures/week_2_assignments/prime_numbers.py
@@ -2,17 +2,15 @@
 prime_numbers = [2]
 number = 2
 
-while True:
+while len(prime_numbers) < 10001:
     is_prime = True
-    # For each number, divide it by each number in list of prime number less than it
+    # For each number, divide it by each number in list of prime number less than its square root
     for i in prime_numbers:
         if number % i == 0:
             # If number is divisible by any number already in list of prime numbers, it is not prime
             # Stop for loop to check for prime
             is_prime = False
             break
-        # If number is not divisible by each number in list of prime numbers, increment i by 1
-        i += 1
 
     # If number is not divisible by any number in list of prime numbers,
     # is_prime should have value True. Add it to the list.
@@ -23,9 +21,6 @@ while True:
     # While loop starts again for the next number
     number += 1
 
-    # If length of prime number list reaches 10001, stop while loop
-    if len(prime_numbers) == 10001:
-        break
 
 # Return the 10001st prime number (last item in list of 10001 items)
 print(prime_numbers[-1])
