4e76c2613dc6fd6bd3ec4c8e14ce368f093a1733
EllieBui <ellie@gotitapp.co>
2020-05-11 21:37:25 +0700

    fix: Refactor add_block function in Problem 5

--- data_structures/weekly_project/task_5.py
+++ data_structures/weekly_project/task_5.py
@@ -22,36 +22,34 @@ class BlockChain:
     def __init__(self):
         self.head = None
 
-    def add_block(self, new_block):
+    def add_block(self, data):
         # if block chain is empty
+        # create first block
         if self.head is None:
+            new_block = Block(timestamp=datetime.datetime.utcnow(),
+                              data=data,
+                              previous_hash=None)
+
             self.head = new_block
             return
+
+        # create new block
+        new_block = Block(timestamp=datetime.datetime.utcnow(),
+                          data=data,
+                          previous_hash=None)
 
         block = self.head
         while block.next:
             block = block.next
         block.next = new_block
+        new_block.previous_hash = block.hash
 
 
 block_chain = BlockChain()
-block_1 = Block(timestamp=datetime.datetime.utcnow(),
-                data="a1",
-                previous_hash=None)
-block_2 = Block(timestamp=datetime.datetime.utcnow(),
-                data="a2",
-                previous_hash=block_1.hash)
-block_3 = Block(timestamp=datetime.datetime.utcnow(),
-                data="a3",
-                previous_hash=block_2.hash)
-block_4 = Block(timestamp=datetime.datetime.utcnow(),
-                data="a4",
-                previous_hash=block_3.hash)
-
-block_chain.add_block(block_1)
-block_chain.add_block(block_2)
-block_chain.add_block(block_3)
-block_chain.add_block(block_4)
+block_chain.add_block("a1")
+block_chain.add_block("a2")
+block_chain.add_block("a3")
+block_chain.add_block("a4")
 
 block = block_chain.head
 
@@ -69,28 +67,27 @@ print(block.data)
 print(block.previous_hash)
 print(block.hash)
 
-
 """
 Answer:
-2020-05-11 14:23:18.381751
+2020-05-11 14:36:21.147749
 a1
 None
 f55ff16f66f43360266b95db6f8fec01d76031054306ae4a4b380598f6cfd114
 
 
-2020-05-11 14:23:18.381941
+2020-05-11 14:36:21.147931
 a2
 f55ff16f66f43360266b95db6f8fec01d76031054306ae4a4b380598f6cfd114
 2c3a4249d77070058649dbd822dcaf7957586fce428cfb2ca88b94741eda8b07
 
 
-2020-05-11 14:23:18.381947
+2020-05-11 14:36:21.147938
 a3
 2c3a4249d77070058649dbd822dcaf7957586fce428cfb2ca88b94741eda8b07
 f46dd28a5499d8efef0b8fb8ee1ec1c5a5e407c9381741d576ba8deb4f59ec3f
 
 
-2020-05-11 14:23:18.381950
+2020-05-11 14:36:21.147942
 a4
 f46dd28a5499d8efef0b8fb8ee1ec1c5a5e407c9381741d576ba8deb4f59ec3f
 4539e4b4889079c2a00afeae0bfc1439840ef2379a1fb81c8ba27361ad476d6b
